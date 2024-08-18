e5646c6f7dec2fdf69a010358a69213afb15fda4
abdallaflood <wtuewtu@gmail.com>
2024-08-18 16:11:26 +0300

    last

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,56 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SharedRichText extends Schema.Component {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    displayName: 'rich-text';
+  };
+  attributes: {
+    rich_text: Attribute.RichText & Attribute.Required;
+  };
+}
+
+export interface SharedNav extends Schema.Component {
+  collectionName: 'components_shared_navs';
+  info: {
+    displayName: 'nav';
+    icon: 'database';
+  };
+  attributes: {
+    name: Attribute.String & Attribute.Required;
+    url: Attribute.String & Attribute.Required;
+  };
+}
+
+export interface SharedImage extends Schema.Component {
+  collectionName: 'components_shared_images';
+  info: {
+    displayName: 'image';
+  };
+  attributes: {
+    image: Attribute.Media<'images'> & Attribute.Required;
+  };
+}
+
+export interface HomepageHero extends Schema.Component {
+  collectionName: 'components_homepage_heroes';
+  info: {
+    displayName: 'hero';
+  };
+  attributes: {
+    sub_title: Attribute.String & Attribute.Required;
+    name: Attribute.String & Attribute.Required;
+    job_title: Attribute.String & Attribute.Required;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'shared.rich-text': SharedRichText;
+      'shared.nav': SharedNav;
+      'shared.image': SharedImage;
+      'homepage.hero': HomepageHero;
+    }
+  }
 }
